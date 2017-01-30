82cd9f071001fb65e75f783f946b930a77bae672
Roger Jönsson <rogerjoensson@outlook.com>
2017-01-30 09:25:33 +0100

    First commit

--- DecoForm/Form1.Designer.vb
+++ DecoForm/Form1.Designer.vb
@@ -22,9 +22,153 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.setDecoParamButton = New System.Windows.Forms.Button()
+        Me.clearDecoParamButton = New System.Windows.Forms.Button()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Black", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(518, 38)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Decoration Specification Dashboard"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.BackColor = System.Drawing.SystemColors.Info
+        Me.RichTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.RichTextBox1.Location = New System.Drawing.Point(12, 522)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(635, 103)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = ""
+        '
+        'setDecoParamButton
+        '
+        Me.setDecoParamButton.Font = New System.Drawing.Font("Segoe UI", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.setDecoParamButton.Location = New System.Drawing.Point(19, 73)
+        Me.setDecoParamButton.Name = "setDecoParamButton"
+        Me.setDecoParamButton.Size = New System.Drawing.Size(232, 53)
+        Me.setDecoParamButton.TabIndex = 2
+        Me.setDecoParamButton.Text = "Set Deco Spec Parameter"
+        Me.setDecoParamButton.UseVisualStyleBackColor = True
+        '
+        'clearDecoParamButton
+        '
+        Me.clearDecoParamButton.Font = New System.Drawing.Font("Segoe UI", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.clearDecoParamButton.Location = New System.Drawing.Point(19, 452)
+        Me.clearDecoParamButton.Name = "clearDecoParamButton"
+        Me.clearDecoParamButton.Size = New System.Drawing.Size(253, 53)
+        Me.clearDecoParamButton.TabIndex = 3
+        Me.clearDecoParamButton.Text = "Clear Deco Spec Parameter"
+        Me.clearDecoParamButton.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button5)
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Segoe UI", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(19, 132)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(628, 314)
+        Me.GroupBox1.TabIndex = 4
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Coloring Actions"
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(62, 62)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(55, 55)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "G"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(214, 62)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(55, 55)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "R"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.Location = New System.Drawing.Point(137, 62)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(55, 55)
+        Me.Button3.TabIndex = 2
+        Me.Button3.Text = "Y"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.Location = New System.Drawing.Point(62, 144)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(207, 55)
+        Me.Button4.TabIndex = 3
+        Me.Button4.Text = "White back"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.Location = New System.Drawing.Point(62, 229)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(207, 55)
+        Me.Button5.TabIndex = 4
+        Me.Button5.Text = "Clear Appearance"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(659, 637)
+        Me.Controls.Add(Me.setDecoParamButton)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.clearDecoParamButton)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Decorations Specification Dashboard"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents Label1 As Label
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents setDecoParamButton As Button
+    Friend WithEvents clearDecoParamButton As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
 End Class
--- DecoForm/Form1.vb
+++ DecoForm/Form1.vb
@@ -1,3 +1,106 @@
-﻿Public Class Form1
+﻿Imports pfcls
+Public Class Form1
+
+
+
+    Public Sub setParameter()
+        Dim asyncConnection As IpfcAsyncConnection = Nothing
+        Dim model As IpfcModel
+        Dim activeserver As IpfcServer
+        Dim paramown As IpfcParameterOwner
+        Dim ipparam As IpfcParameter
+        Dim ipbaseparam As IpfcBaseParameter
+        Dim paramval As IpfcParamValue
+        Dim Paraname As String = "SURFACE_FINISH"
+
+        Dim session As IpfcBaseSession
+        Dim Moditem As CMpfcModelItem
+
+        Try
+            asyncConnection = (New CCpfcAsyncConnection).Connect(Nothing, Nothing, Nothing, Nothing)
+            session = asyncConnection.Session
+            activeserver = session.GetActiveServer
+            model = session.CurrentModel
+
+            If model Is Nothing Then
+                MsgBox("Model is not present",, "Script message")
+                Environment.Exit(0)
+            End If
+
+            If (Not model.Type = EpfcModelType.EpfcMDL_PART) And (Not model.Type = EpfcModelType.EpfcMDL_ASSEMBLY) Then
+                MsgBox("Model is not a solid",, "Script message")
+                Environment.Exit(0)
+            End If
+
+            If Not activeserver.IsObjectCheckedOut(activeserver.ActiveWorkspace, model.FileName) Then
+                MsgBox("Please check out model first...",, "Script Message")
+                Environment.Exit(0)
+            End If
+
+            paramown = model
+            ipparam = paramown.GetParam(Paraname)
+            ipbaseparam = ipparam
+            Moditem = New CMpfcModelItem
+
+            paramval = Moditem.CreateStringParamValue(CStr("SEE DECORATION SPECIFICATION"))
+            ipbaseparam.Value = paramval
+
+            MsgBox("Surface finish has been set to 'SEE DECORATION SPECIFICATION'",, "Script Message")
+            asyncConnection.Disconnect(1)
+
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Public Sub clearParameter()
+        Dim asyncConnection As IpfcAsyncConnection = Nothing
+        Dim model As IpfcModel
+        Dim activeserver As IpfcServer
+        Dim paramown As IpfcParameterOwner
+        Dim ipparam As IpfcParameter
+        Dim ipbaseparam As IpfcBaseParameter
+        Dim paramval As IpfcParamValue
+        Dim Paraname As String = "SURFACE_FINISH"
+
+        Dim session As IpfcBaseSession
+        Dim Moditem As CMpfcModelItem
+
+        Try
+            asyncConnection = (New CCpfcAsyncConnection).Connect(Nothing, Nothing, Nothing, Nothing)
+            session = asyncConnection.Session
+            activeserver = session.GetActiveServer
+            model = session.CurrentModel
+
+
+            If model Is Nothing Then
+                MsgBox("Model is not present",, "Script message")
+                Environment.Exit(0)
+            End If
+
+            If (Not model.Type = EpfcModelType.EpfcMDL_PART) And (Not model.Type = EpfcModelType.EpfcMDL_ASSEMBLY) Then
+                MsgBox("Model is not a solid",, "Script message")
+                Environment.Exit(0)
+            End If
+
+            If Not activeserver.IsObjectCheckedOut(activeserver.ActiveWorkspace, model.FileName) Then
+                MsgBox("Please check out model first...",, "Script Message")
+                Environment.Exit(0)
+            End If
+
+            paramown = model
+            ipparam = paramown.GetParam(Paraname)
+            ipbaseparam = ipparam
+
+            Moditem = New CMpfcModelItem
+
+            paramval = Moditem.CreateStringParamValue(CStr("-"))
+            ipbaseparam.Value = paramval
+
+            MsgBox("Surface finish has been set to '-'",, "Script Message")
+            asyncConnection.Disconnect(1)
+
+        Catch ex As Exception
+        End Try
+    End Sub
 
 End Class
